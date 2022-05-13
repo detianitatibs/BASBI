f7cf2aaa111936cc132c3e978b8a2754b86ecfdb
Keisuke Tokuda <tokuda.keisuke.pm@gmail.com>
2022-05-14 01:13:36 +0900

    hotfix#3確認用のテストデータを追加

--- data_collect/tests/conftest.py
+++ data_collect/tests/conftest.py
@@ -13,3 +13,33 @@ def init_r_html_01() -> HTML:
         html = f.read()
         r_html = HTML(html=html)
     return r_html
+
+@pytest.fixture(scope='class')
+def init_r_html_02() -> HTML:
+    base_dir = os.path.dirname(__file__)
+    sample_path = '/'.join([base_dir, 'data/sample_stats_schedule_key_4094.txt'])
+    # htmlファイルの読み込み
+    with open(sample_path) as f:
+        html = f.read()
+        r_html = HTML(html=html)
+    return r_html
+
+@pytest.fixture(scope='class')
+def init_r_html_03() -> HTML:
+    base_dir = os.path.dirname(__file__)
+    sample_path = '/'.join([base_dir, 'data/sample_stats_schedule_key_4113.txt'])
+    # htmlファイルの読み込み
+    with open(sample_path) as f:
+        html = f.read()
+        r_html = HTML(html=html)
+    return r_html
+
+@pytest.fixture(scope='class')
+def init_r_html_04() -> HTML:
+    base_dir = os.path.dirname(__file__)
+    sample_path = '/'.join([base_dir, 'data/sample_stats_schedule_key_4118.txt'])
+    # htmlファイルの読み込み
+    with open(sample_path) as f:
+        html = f.read()
+        r_html = HTML(html=html)
+    return r_html
